fab5eb707390c9149b42bc7f10aea10b3e531652
enere1 <enere1@naver.com>
2024-07-06 16:53:11 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:delivery_app/common/default_layout.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+        home: DefaultLayout()
+    )
+  );
+}
