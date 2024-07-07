a83a4a8c1784ca88d979d995be52a0a768d8d9d1
leesungon <enere1@naver.com>
2024-07-07 23:13:25 +0900

    [FEAT] providerスコープ設定

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,16 @@ import 'package:delivery_app/common/view/root_tab.dart';
 import 'package:delivery_app/user/view/login.dart';
 import 'package:delivery_app/user/view/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-        theme: ThemeData(
-          fontFamily: 'Notosans',
-        ),
-        home: RootTab()
-    )
+    ProviderScope(
+      child: MaterialApp(
+          theme: ThemeData(
+            fontFamily: 'Notosans',
+          ),
+          home: RootTab()),
+    ),
   );
 }
